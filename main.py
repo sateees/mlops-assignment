e4d81e02377964bbed2e4929db7d0a0250bb7bde
sateees <nst0106@gmail.com>
2025-01-25 23:04:19 +0530

    display model name

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def train_with_mlflow():
         mlflow.sklearn.log_model(trainer.pipeline, "model")
                 
         # Register the model
-        model_name = "insurance_model" 
+        model_name = config['model']['name'] 
         model_uri = f"runs:/{run.info.run_id}/model"
         mlflow.register_model(model_uri, model_name)
 
